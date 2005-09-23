11f158194ff2b18ed40a10318344fb2a96626819
Tommi Virtanen <tv@inoi.fi>
2005-09-23 14:20:00 +0300

    Web testing with pbp, with near-perfect coverage.

---
+++ eocmanage/test/util/pbputil.py
@@ -0,0 +1,18 @@
+import os, shutil, errno
+
+def destroy(name):
+    """Destroy list with given name"""
+    print 'DESTROY', repr(name)
+    assert '@' in name
+    assert not name.startswith('.')
+    path = os.path.join('dot-eoc', name)
+    try:
+        shutil.rmtree(path)
+    except OSError, e:
+        if e.errno == errno.ENOENT:
+            pass
+        else:
+            raise
+
+__pbp__ = ['destroy',
+           ]
